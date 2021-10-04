02b33f30cb5b9370f9a6fd4105fe674614706336
Your Name <bruna@fedora.(none)>
2021-10-04 16:21:37 -0300

    template.js

---
+++ modulo-1/variaveis/template_javascript/index.js
@@ -0,0 +1,43 @@
+/* Interpretação de código
+Execicio 1
+console.log (b)=10
+console.log(a, b)=10, 5
+Exercicio 2
+console.log(a,b,c)= 10,10,10
+Exercicio 3
+p= HorasTrabalhadas
+t=GanhoDiario*/ 
+
+//Escrita de Código
+//Crinado variáveis
+ let nomeEstudante
+let idadeEstudante
+console.log (typeof nomeEstudante)
+console.log (typeof idadeEstudante) 
+//Resposta: undefined
+//Atribuindo nomes
+ let nomeEstudante= prompt ('Qual o seu nome?')
+let idadeEstudante= prompt ('Qual sua idade?')
+console.log('Ola',nomeEstudante, 'você tem', 
+idadeEstudante, 'anos')  
+//Resposta:Apareceu uma caixa de perguntas e minha resposta ficou gravada no console. 
+//Pergunta sim ou não
+const beberAgua= prompt ('Você bebeu água hoje?')
+const cafeDaManha= prompt ('Você tomou muito café?')
+const carinhoNoPet= prompt ('Já fez um cafuné no seu pet hoje?')
+console.log(beberAgua)
+console.log (cafeDaManha)
+console.log(carinhoNoPet)  
+//Mudando os numeros
+let a=25
+let b=10
+c=b
+b=a
+console.log(a,b,c)  
+//Resposta:25,25,10
+//Desafio
+ let valorA = Number (prompt('Insira um numemro'))
+let valorB = Number(prompt('Insira um numero'))
+console.log (valorA+valorB)
+console.log (valorA*valorB) 
+
