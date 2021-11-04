116bca2fbbf5763aeb5dbc5862dc047baab879b8
Your Name <bruna@fedora.(none)>
2021-11-04 17:11:51 -0300

    estatos-insta4

---
+++ modulo-2/estados-insta4/insta4/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'Paulinha'}
+          fotoUsuario={'https://i.picsum.photos/id/1006/3000/2000.jpg?hmac=x83pQQ7LW1UTo8HxBcIWuRIVeN_uCg0cG6keXvNvM8g'}
+          fotoPost={'https://i.picsum.photos/id/102/4320/3240.jpg?hmac=ico2KysoswVG8E8r550V_afIWN963F6ygTVrqHeHeRc'}
+        />
+        <Post
+          nomeUsuario={'Juliana'}
+          fotoUsuario={'https://i.picsum.photos/id/1001/5616/3744.jpg?hmac=38lkvX7tHXmlNbI0HzZbtkJ6_wpWyqvkX4Ty6vYElZE'}
+          fotoPost={'https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY'}
+        />
+        <Post
+          nomeUsuario={'Eloísa'}
+          fotoUsuario={'https://i.picsum.photos/id/1005/5760/3840.jpg?hmac=2acSJCOwz9q_dKtDZdSB-OIK1HUcwBeXco_RMMTUgfY'}
+          fotoPost={'https://i.picsum.photos/id/1002/4312/2868.jpg?hmac=5LlLE-NY9oMnmIQp7ms6IfdvSUQOzP_O3DPMWmyNxwo'}
+        />
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
